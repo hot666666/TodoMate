bc998f646829ca0d7d646692b4a742f78a1b67b4
hot666666 <hotcs6@naver.com>
2024-08-12 12:22:29 +0900

    Initial Commit

---
+++ TodoMate/TodoMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMateApp.swift
+//  TodoMate
+//
+//  Created by hs on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
