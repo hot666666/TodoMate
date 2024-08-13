a500cc875176e731dee6f0c3c84cdcda4264bd87
hot666666 <hotcs6@naver.com>
2024-08-13 12:58:07 +0900

    fix : TodoItem model

--- TodoMate/Model/TodoItem.swift
+++ TodoMate/Model/TodoItem.swift
@@ -6,57 +6,62 @@
 //
 
 import SwiftUI
+import SwiftData
+
+enum TodoItemStatus: String, Codable, CaseIterable {
+    case todo = "시작 전"
+    case inProgress = "진행 중"
+    case complete = "완료"
+    case inComplete = "미완료"
+    
+    var color: Color {
+        switch self {
+        case .todo: return .gray
+        case .inProgress: return .blue
+        case .complete: return .green
+        case .inComplete: return .red
+        }
+    }
+}
 
 @Observable
 class TodoItem: Identifiable {
-    enum Status: String, CaseIterable {
-        case todo = "시작 전"
-        case inProgress = "진행 중"
-        case complete = "완료"
-        case inComplete = "미완료"
-        
-        var color: Color {
-            switch self {
-            case .todo: return .gray
-            case .inProgress: return .blue
-            case .complete: return .green
-            case .inComplete: return .red
-            }
-        }
-    }
-    
     var id: String
     var date: Date
     var content: String
-    var status: Status
+    var status: TodoItemStatus
     var detail: String
     var startTime: Date?
     var endTime: Date?
+    var pid: PersistentIdentifier?
     
-    init(){
+    init(date: Date = .now,
+         content: String = "이름없음",
+         detail: String = "",
+         status: TodoItemStatus = .todo,
+         startTime: Date? = nil,
+         endTime: Date? = nil,
+         pid: PersistentIdentifier? = nil)
+    {
         self.id = UUID().uuidString
-        self.date = .now
-        self.content = "이름없음"
-        self.status = .todo
-        self.detail = ""
-    }
-    
-    init(id: String, date: Date, content: String, detail: String = "", status: Status, startTime: Date? = nil, endTime: Date? = nil) {
-        self.id = id
         self.date = date
         self.content = content
         self.detail = detail
         self.status = status
         self.startTime = startTime
         self.endTime = endTime
+        self.pid = pid
     }
 }
 
+extension TodoItem {
+    func toEntity() -> TodoItemEntity {
+        TodoItemEntity(content: self.content, date: self.date, status: self.status, detail: self.detail)
+    }
+}
 
 extension TodoItem {
-    static var stub: [TodoItem] {
-        [.init(id: UUID().uuidString, date: .now, content: "할 일1", status: .todo),
-         .init(id: UUID().uuidString, date: .now, content: "할 일2", status: .todo),
-         .init(id: UUID().uuidString, date: .now, content: "할 일3", status: .todo)]
+    static var stub: TodoItem {
+        .init()
     }
 }
