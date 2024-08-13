23a590bd69018c862edb8cdf974ee53ba2d1b9ca
hot666666 <hotcs6@naver.com>
2024-08-13 13:01:34 +0900

    fix : CustomSheetModifier Bug - 화면 전체에 해당 수정자 적용 - create : AppState

--- TodoMate/View/ContentView.swift
+++ TodoMate/View/ContentView.swift
@@ -6,15 +6,40 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext: ModelContext
+    @State private var appState: AppState = .init()
+    @State private var todoItemManager: TodoItemManager = .init(todoItemRepository: .init())
+    
     var body: some View {
-        TodoListView()
-            .padding()
+        VStack{
+            TodoListView()
+                .padding()
+                .environment(appState)
+                .environment(todoItemManager)
+                .onAppear {
+                    todoItemManager.fetch(modelContext: modelContext)
+                }
+            
+//            RoundedRectangle(cornerRadius: 10)
+//                .padding()
+//                .overlay {
+//                    Text("달력")
+//                }
+            
+        }
+        /// MacOS 앱은 기본적으로 .sheet를 이용할 때, 외부 뷰 터치 시 dismiss가 수행을 안해서 따로 만든 커스텀 수정자
+        .customSheet(selectedItem: $appState.selectedTodoItem) { todo in
+            TodoListSheetView(todo: todo) { updateTodo in
+                todoItemManager.update(modelContext: modelContext, updateTodo)
+            }
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sampleData) {
     ContentView()
         .frame(minWidth: 300, minHeight: 500)
 }
