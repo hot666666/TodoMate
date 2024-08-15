920ce35a4767bc34b6b570077c38093580f3d299
hot666666 <hotcs6@naver.com>
2024-08-15 12:55:48 +0900

    fix : Update appState usage in ContentView and TodoMateApp

--- TodoMate/TodoMateApp.swift
+++ TodoMate/TodoMateApp.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 @main
 struct TodoMateApp: App {
+    @State private var appState: AppState = .init()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .modelContainer(for: TodoItemEntity.self)
+                .environment(appState)
         }
     }
 }
