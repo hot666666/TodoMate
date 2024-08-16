7d37097e341569ac179126dc7b9b2d3b6cddccb6
hot666666 <hotcs6@naver.com>
2024-08-16 17:08:12 +0900

    feat : add Color extension - Color Assets -> Color extension

---
+++ TodoMate/Extension/Color+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extension.swift
+//  TodoMate
+//
+//  Created by hs on 8/16/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customBlue = Color(red: 0x28 / 255, green: 0x45 / 255, blue: 0x6C / 255)
+    static let customGray = Color(red: 0x5A / 255, green: 0x5A / 255, blue: 0x5A / 255)
+    static let customGreen = Color(red: 0x2B / 255, green: 0x59 / 255, blue: 0x3F / 255)
+    static let customRed = Color(red: 0x69 / 255, green: 0x31 / 255, blue: 0x4C / 255)
+}
