4b94f27fc6638bf918e4b3e364f7f2bc1959f89c
hot666666 <hotcs6@naver.com>
2024-08-16 17:05:17 +0900

    feat : add Realtime listener for TodoManager - update FirestoreTodoRepository - AsyncStream을 이용한 DatabaseChange<TodoDTO> 전달을 이용

--- TodoMate/Helper/TodoManager.swift
+++ TodoMate/Helper/TodoManager.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 protocol TodoManagerProtocol {
+    var todos: [Todo] { get set }
     func create(_ todo: Todo) async
     func fetch() async
     func update(_ todo: Todo)
@@ -17,24 +19,54 @@ protocol TodoManagerProtocol {
 @Observable
 class TodoManager: TodoManagerProtocol {
     var todos: [Todo] = []
-
+    
     private let todoRepository: TodoRepository
-    private var userId: String
+    private var task: Task<Void, Never>?
     
-    
-    init(userId: String,
-         todoRepository: TodoRepository = FirestoreTodoRepository(reference: .shared)) {
-        self.userId = userId
+    init(todoRepository: TodoRepository = FirestoreTodoRepository(reference: .shared)) {
         self.todoRepository = todoRepository
+        setupRealtimeUpdates()
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+
+}
+
+extension TodoManager {
+    private func setupRealtimeUpdates() {
+        task = Task {
+            for await change in todoRepository.observeTodoChanges() {
+                print("[Observed change in FirebaseFirestore] - ", change)
+                await handleDatabaseChange(change)
+            }
+        }
+    }
+    
+    @MainActor
+    private func handleDatabaseChange(_ change: DatabaseChange<TodoDTO>) {
+        switch change {
+        case .added(let todoDTO):
+            let todo = todoDTO.toModel()
+            if !todos.contains(where: { $0.fid == todo.fid }) {
+                todos.append(todo)
+            }
+        case .modified(let todoDTO):
+            if let index = todos.firstIndex(where: { $0.fid == todoDTO.id }) {
+                todos[index] = todoDTO.toModel()
+            }
+        case .removed(let id):
+            todos.removeAll { $0.fid == id }
+        }
     }
 }
 
 extension TodoManager {
-    
     @MainActor
     func fetch() async {
         do {
-            todos = try await todoRepository.fetchTodos(userId: userId).map { $0.toModel() }
+            todos = try await todoRepository.fetchTodos().map { $0.toModel() }
         } catch {
             print("Error fetching todos: \(error)")
         }
@@ -44,7 +76,7 @@ extension TodoManager {
         Task {
             guard let todoId = todo.fid else { return }
             do {
-                try await todoRepository.deleteTodo(userId: userId, todoId: todoId)
+                try await todoRepository.deleteTodo(todoId: todoId)
             } catch {
                 print("Error deleting todo: \(error)")
             }
@@ -54,7 +86,7 @@ extension TodoManager {
     func update(_ todo: Todo) {
         Task {
             do {
-                try await todoRepository.updateTodo(userId: userId, todo: todo.toDTO())
+                try await todoRepository.updateTodo(todo: todo.toDTO())
             } catch {
                 print("Error updating todo: \(error)")
             }
@@ -64,10 +96,39 @@ extension TodoManager {
     @MainActor
     func create(_ todo: Todo) async {
         do {
-            let id = try await todoRepository.createTodo(userId: userId, todo: todo.toDTO())
+            let id = try await todoRepository.createTodo(todo: todo.toDTO())
             todo.fid = id
         } catch {
             print("Error creating todo: \(error)")
         }
+    }
+}
+
+@Observable
+class StubTodoManager: TodoManagerProtocol {
+    var todos: [Todo]
+    
+    init(todos: [Todo] = []) {
+        self.todos = todos
+    }
+    
+    func fetch() async {
+        todos = Todo.stub
+    }
+    
+    func remove(_ todo: Todo) {
+        todos.removeAll { $0.fid == todo.fid }
+    }
+    
+    func update(_ todo: Todo) {
+        if let index = todos.firstIndex(where: { $0.fid == todo.fid }) {
+            todos[index] = todo
+        }
+    }
+    
+    @MainActor
+    func create(_ todo: Todo) async {
+        todo.fid = UUID().uuidString /// Firebase에 업데이트되면 얻는 고유 ID 과정
+        todos.append(todo)
     }
 }
--- TodoMate/Repository/FirestoreTodoRepository.swift
+++ TodoMate/Repository/FirestoreTodoRepository.swift
@@ -7,11 +7,18 @@
 
 import FirebaseFirestore
 
+enum DatabaseChange<T> {
+    case added(T)
+    case modified(T)
+    case removed(String)
+}
+
 protocol TodoRepository {
-    func fetchTodos(userId: String) async throws -> [TodoDTO]
-    func createTodo(userId: String, todo: TodoDTO) async throws -> String
-    func updateTodo(userId: String, todo: TodoDTO) async throws
-    func deleteTodo(userId: String, todoId: String) async throws
+    func fetchTodos() async throws -> [TodoDTO]
+    func createTodo(todo: TodoDTO) async throws -> String
+    func updateTodo(todo: TodoDTO) async throws
+    func deleteTodo(todoId: String) async throws
+    func observeTodoChanges() -> AsyncStream<DatabaseChange<TodoDTO>>
 }
 
 class FirestoreTodoRepository: TodoRepository {
@@ -21,9 +28,42 @@ class FirestoreTodoRepository: TodoRepository {
         self.reference = reference
     }
     
-    func fetchTodos(userId: String) async throws -> [TodoDTO] {
-        let snapshot = try await reference.todoCollection(userId: userId).getDocuments()
-        
+    func observeTodoChanges() -> AsyncStream<DatabaseChange<TodoDTO>> {
+        AsyncStream { continuation in
+            let listener = reference.todoCollection().addSnapshotListener { querySnapshot, error in
+                guard let snapshot = querySnapshot else {
+                    if let error = error {
+                        print("Error fetching snapshots: \(error)")
+                    }
+                    return
+                }
+                
+                snapshot.documentChanges.forEach { diff in
+                    if let todoDTO = try? diff.document.data(as: TodoDTO.self) {
+                        switch diff.type {
+                        case .added:
+                            continuation.yield(.added(todoDTO))
+                        case .modified:
+                            continuation.yield(.modified(todoDTO))
+                        case .removed:
+                            continuation.yield(.removed(diff.document.documentID))
+                        @unknown default:
+                            break
+                        }
+                    }
+                }
+            }
+            
+            continuation.onTermination = { @Sendable _ in
+                listener.remove()
+            }
+        }
+    }
+}
+
+extension FirestoreTodoRepository {
+    func fetchTodos() async throws -> [TodoDTO] {
+        let snapshot = try await reference.todoCollection().getDocuments()
         return snapshot.documents.compactMap { document -> TodoDTO? in
             do {
                 return try document.data(as: TodoDTO.self)
@@ -34,22 +74,22 @@ class FirestoreTodoRepository: TodoRepository {
         }
     }
     
-    func createTodo(userId: String, todo: TodoDTO) async throws -> String {
-        let todoDocRef = reference.todoCollection(userId: userId).document()
+    func createTodo(todo: TodoDTO) async throws -> String {
+        let todoDocRef = reference.todoCollection().document()
         var newTodo = todo
         newTodo.id = todoDocRef.documentID
         try todoDocRef.setData(from: newTodo)
         return todoDocRef.documentID
     }
     
-    func updateTodo(userId: String, todo: TodoDTO) async throws {
+    func updateTodo(todo: TodoDTO) async throws {
         guard let todoId = todo.id else { return }
-        let todoDocRef = reference.todoCollection(userId: userId).document(todoId)
+        let todoDocRef = reference.todoCollection().document(todoId)
         try todoDocRef.setData(from: todo)
     }
     
-    func deleteTodo(userId: String, todoId: String) async throws {
-        let todoDocRef = reference.todoCollection(userId: userId).document(todoId)
+    func deleteTodo(todoId: String) async throws {
+        let todoDocRef = reference.todoCollection().document(todoId)
         try await todoDocRef.delete()
     }
 }
