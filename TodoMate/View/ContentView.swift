d5e8b811076955ab8cc812f7949016187c30d0e9
hot666666 <hotcs6@naver.com>
2024-08-22 10:42:20 +0900

    feat : create TodoListViewModel, update ContentView - TodoListView, TodoListViewModel - ContentView

--- TodoMate/View/ContentView.swift
+++ TodoMate/View/ContentView.swift
@@ -8,32 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(DIContainer.self) private var container: DIContainer
     @Environment(AppState.self) private var appState: AppState
     @State private var userManager: UserManager = .init()
-    @State private var todoManager: TodoManager = .init()
     @State private var chatManager: ChatManager = .init()
-    
     
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
-                // TODO: - 최하단 채팅으로 스크롤 이동
-                ExpandableView(title: "채팅\(chatManager.formatCount)", isExpanded: false) {
-                    ChatListView()
-                        .padding(.horizontal, 5)
-                        .environment(chatManager)
-                }
-                
-                ForEach(userManager.users) { user in
-                    ExpandableView(title: user.name) {
-                        TodoListView(userId: user.fid)
-                            .padding()
-                            .frame(height: max(50, geometry.size.height / 2 - 100))
+                VStack {
+                    ExpandableView(title: "채팅\(chatManager.formatCount)") {
+                        ChatListView()
+                            .padding(.horizontal, 5)
+                            .environment(chatManager)
                     }
+                    .padding(.top)
+                    
+                    ForEach(userManager.users) { user in
+                        ExpandableView(title: user.name, isExpanded: true) {
+                            ExpandableView(title: "캘린더") {
+                                TodosInMonthView(viewModel: .init(container: container, userId: user.fid))
+                            }
+                            .padding(.horizontal)
+                            
+                            TodoListView(viewModel: .init(container: container, userId: user.fid))
+                                .padding()
+                                .frame(height: max(50, geometry.size.height / 2 - 150))
+                        }
+                    }
+                    .padding(.top, 5)
                 }
-                .padding(.top, 5)
+                .disabled(appState.isSelectedTodo)
+                .padding(.horizontal, max(10, geometry.size.width * 0.1))
             }
-            .padding(5)
         }
         .task {
             await userManager.fetch()
@@ -44,7 +51,6 @@ struct ContentView: View {
         .customOverlayView(isPresented: appState.popover, overlayPosition: appState.popoverPosition) {
             DateSettingView()
         }
-        .environment(todoManager)
     }
 }
 
---
+++ TodoMate/View/TodoListView/TodoListViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  TodoListViewModel.swift
+//  TodoMate
+//
+//  Created by hs on 8/21/24.
+//
+
+import SwiftUI
+
+@Observable
+class TodoListViewModel {
+    private let container: DIContainer
+    private let userId: String
+    private let calendar: Calendar = .current
+    var todos: [Todo] = []
+    
+    init(
+        container: DIContainer,
+        userId: String
+    ) {
+        self.container = container
+        self.userId = userId
+    }
+    
+    func onAppear() {
+        container.todoRealtimeService.addObserver(self, for: userId)
+    }
+    
+    func onDisappear() {
+        container.todoRealtimeService.removeObserver(self, for: userId)
+    }
+}
+
+extension TodoListViewModel {
+    func create() {
+        container.todoService.create(with: userId)
+    }
+    
+    @MainActor
+    func fetch() async {
+        todos = await  container.todoService.fetchToday(userId: userId)
+    }
+    
+    func update(_ todo: Todo) {
+        container.todoService.update(todo)
+    }
+    
+    func remove(_ todo: Todo) {
+        container.todoService.remove(todo)
+    }
+}
+
+extension TodoListViewModel {
+    func move(from source: IndexSet, to destination: Int) {
+        todos.move(fromOffsets: source, toOffset: destination)
+    }
+}
+
+extension TodoListViewModel: TodoObserver {
+    func todoAdded(_ todo: Todo) {
+        guard calendar.isDate(todo.date, inSameDayAs: .now) else { return }
+        todos.append(todo)
+    }
+    
+    func todoModified(_ todo: Todo) {
+        guard calendar.isDate(todo.date, inSameDayAs: .now) else { return }
+        if let index = todos.firstIndex(where: { $0.fid == todo.fid }) {
+            todos[index] = todo
+        }
+    }
+    
+    func todoRemoved(_ todo: Todo) {
+        guard calendar.isDate(todo.date, inSameDayAs: .now) else { return }
+        self.todos.removeAll { $0.fid == todo.fid }
+    }
+}
