105e370d2347efdca01c4f5754725cf8393a0e01
hot666666 <hotcs6@naver.com>
2024-08-27 23:15:09 +0900

    build : create ci_pre_xcodebuild

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+//  ci_pre_xcodebuild.sh
+//  TodoMate
+//
+//  Created by hs on 8/27/24.
+//
+
+# 파일 경로 설정
+PLIST_PATH="${PROJECT_DIR}/GoogleService-Info.plist"
+
+# 파일이 존재하는지 확인
+if [ ! -f "$PLIST_PATH" ]; then
+  echo "Creating GoogleService-Info.plist from environment variable"
+  echo "$GOOGLE_SERVICE_INFO" > "$PLIST_PATH"
+else
+  echo "GoogleService-Info.plist already exists"
+fi
