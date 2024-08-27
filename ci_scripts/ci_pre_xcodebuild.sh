eedd204a564a8a7bbcbcc5b8af435be5034f5144
hot666666 <hotcs6@naver.com>
2024-08-27 23:58:38 +0900

    fix : update ci_pre_xcodebuild typo

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 파일 경로 설정
-PLIST_PATH="${CI_PRIMARY_REPOSITORY_PATH}/${TodoMate}/GoogleService-Info.plist"
+PLIST_PATH="${CI_PRIMARY_REPOSITORY_PATH}/TodoMate/GoogleService-Info.plist"
 
 # 파일이 존재하는지 확인
 if [ ! -f "$PLIST_PATH" ]; then
