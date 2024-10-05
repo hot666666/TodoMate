37291bb9c3ae6f13083d0f279a8203a559961da3
hot666666 <hotcs6@naver.com>
2024-10-05 15:20:04 +0900

    fix : update TodoMateApp - add modelContainer, update data with scenePhase

--- TodoMate/TodoMateApp.swift
+++ TodoMate/TodoMateApp.swift
@@ -6,14 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
+import WidgetKit
 import FirebaseFirestore
 import FirebaseCore
 
 @main
 struct TodoMateApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
     @State private var appState: AppState
     @State private var container: DIContainer
+    
+    private var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            TodoEntity.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
     
     init() {
         if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != "1" {
@@ -23,7 +39,7 @@ struct TodoMateApp: App {
         _appState = State(initialValue: AppState())
         _container = State(initialValue: DIContainer(chatService: ChatService(),
                                                      todoService: TodoService(),
-                                                     todoRealtimeService: TodoRealtimeService(),
+                                                     todoRealtimeService: TodoRealtimeService(modelContainer: sharedModelContainer),
                                                      imageUploadService: ImageUploadService()))
     }
     
@@ -32,12 +48,20 @@ struct TodoMateApp: App {
             ContentView()
                 .environment(appState)
                 .environment(container)
+                .onChange(of: scenePhase, { _, newValue in
+                    if newValue != .active {
+                        try? sharedModelContainer.mainContext.save()
+                        WidgetCenter.shared.reloadAllTimelines()
+                    }
+                })
         }
+        .modelContainer(sharedModelContainer)
         .commands {
             CommandGroup(replacing: .appInfo) {
                 Button("Check for Updates") {
                     appDelegate.checkForUpdates()
                 }
             }
-        }    }
+        }
+    }
 }
