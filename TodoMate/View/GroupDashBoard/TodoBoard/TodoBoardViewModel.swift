2625486b84a9f2259b8f8dcb40267f8a631cb8de
hot666666 <hotcs6@naver.com>
2025-01-28 21:28:22 +0900

    feat: add TodoBoardView and associated ViewModels for enhanced todo management

---
+++ TodoMate/View/GroupDashBoard/TodoBoard/TodoBoardViewModel.swift
@@ -0,0 +1,121 @@
+//
+//  TodoBoardViewModel.swift
+//  TodoMate
+//
+//  Created by hs on 1/20/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@Observable
+class TodoBoardViewModel {
+    private let todoStreamProvider: TodoStreamProviderType
+    private let modelContainer: ModelContainer
+    private var observers: [String: [WeakTodoObserver]] = [:]
+    
+    @ObservationIgnored let userInfo: UserInfo
+    
+    init(container: DIContainer, userInfo: UserInfo) {
+        self.todoStreamProvider = container.todoStreamProvider
+        self.modelContainer = container.modelContainer
+        self.userInfo = userInfo
+    }
+    
+    func observeChanges() async {
+        for await change in todoStreamProvider.createTodoStream() {
+            if !observers.isEmpty {
+                print("[Observed Todo change in FirebaseFirestore] - ", change)
+                await handleDatabaseChange(change)
+            }
+        }
+    }
+    
+    @MainActor
+    private func handleDatabaseChange(_ change: DatabaseChange<Todo>) async {
+        guard let observers = observers[change.data.uid] else {
+            print("[No Observer exists]")
+            return
+        }
+        
+        switch change {
+        case .added(let todo):
+            for observer in observers {
+                observer.value?.todoAdded(todo)
+            }
+        case .modified(let todo):
+            /// 위젯 데이터 - 진행 중이면 추가, 아니면 삭제, 이미 존재하는 Todo의 업데이트라면 무시
+            if todo.status == .inProgress {
+                await saveToModelContainer(todo)
+            } else {
+                await deleteFromModelContainer(todo.fid)
+            }
+            
+            for observer in observers {
+                observer.value?.todoModified(todo)
+            }
+        case .removed(let todo):
+            /// 위젯 데이터 - 존재하면 삭제
+            await deleteFromModelContainer(todo.fid)
+            
+            for observer in observers {
+                observer.value?.todoRemoved(todo)
+            }
+        }
+    }
+}
+extension TodoBoardViewModel {
+    @MainActor
+    private func saveToModelContainer(_ todo: Todo) async {
+        /// 본인 것만 위젯에 표시
+        guard todo.uid == userInfo.id else { return }
+        
+        let entity = todo.toEntity()
+        guard let fid = entity.fid else {
+            print("TodoEntity has no fid")
+            return
+        }
+        
+        let context = modelContainer.mainContext
+        
+        let fetchDescriptor = FetchDescriptor<TodoEntity>(predicate: #Predicate { $0.fid == fid })
+        do {
+            if let existingEntity = try context.fetch(fetchDescriptor).first {
+                existingEntity.date = entity.date
+                existingEntity.content = entity.content
+                return
+            }
+            context.insert(entity)
+            print("New TodoEntity inserted with fid \(fid)")
+        } catch {
+            print("Error checking for existing TodoEntity: \(error.localizedDescription)")
+        }
+    }
+    
+    @MainActor
+    private func deleteFromModelContainer(_ todoId: String?) async {
+        guard let todoId else { return }
+        
+        let context = modelContainer.mainContext
+        
+        let fetchDescriptor = FetchDescriptor<TodoEntity>(predicate: #Predicate { $0.fid == todoId })
+        if let existingEntity = try? context.fetch(fetchDescriptor).first {
+            context.delete(existingEntity)
+            print("TodoEntity deleted")
+        }
+    }
+}
+extension TodoBoardViewModel {
+    func addObserver(_ observer: TodoObserverType, for userId: String) {
+        observers[userId, default: []].removeAll(where: { $0.value == nil })
+        print("[Add Observer - \(ObjectIdentifier(observer))]")
+        observers[userId, default: []].append(WeakTodoObserver(observer))
+    }
+    
+    func removeObserver(_ observer: TodoObserverType, for userId: String) {
+        if observers[userId, default: []].contains(where: { $0.value === observer }) {
+            print("[Remove Observer - \(ObjectIdentifier(observer))]")
+        }
+        observers[userId, default: []].removeAll(where: { $0.value === observer || $0.value == nil })
+    }
+}
