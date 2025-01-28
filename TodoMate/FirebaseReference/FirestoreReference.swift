6c7068e4f05eac1f20cd48d4879be20a925dc60c
hot666666 <hotcs6@naver.com>
2025-01-28 21:22:26 +0900

    refactor : Reference - FirestoreReference, FStorageReference

---
+++ TodoMate/FirebaseReference/FirestoreReference.swift
@@ -0,0 +1,36 @@
+//
+//  FireStoreReference.swift
+//  TodoMate
+//
+//  Created by hs on 8/15/24.
+//
+
+import Firebase
+
+#if !PREVIEW
+import FirebaseFirestore
+final class FirestoreReference {
+    static let shared = FirestoreReference()
+    let db = Firestore.firestore()
+    
+    private init() {}
+    
+    func userCollection() -> CollectionReference {
+        return db.collection(FireStore.USER)
+    }
+    
+    func todoCollection() -> CollectionReference {
+        return db.collection(FireStore.TODO)
+    }
+    
+    func chatCollection() -> CollectionReference {
+        return db.collection(FireStore.CHAT)
+    }
+}
+#else
+class FirestoreReference {
+    static let shared = FirestoreReference()
+    
+    private init() {}
+}
+#endif
