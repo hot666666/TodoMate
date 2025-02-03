b35eacd55617dc9739c03a77247f215028845cb5
hot666666 <hotcs6@naver.com>
2025-02-03 16:06:04 +0900

    feat: disable date selection when todo is in progress

--- TodoMate/View/OverlayContainer/TodoSheet/TodoSheetView.swift
+++ TodoMate/View/OverlayContainer/TodoSheet/TodoSheetView.swift
@@ -102,6 +102,7 @@ fileprivate struct TodoSheetDate: View {
                             }
                     }
                 )
+                .disabled(todo.status == .inProgress)
         }
     }
     
