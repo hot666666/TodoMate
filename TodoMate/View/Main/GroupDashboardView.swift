71b5890217229a40518c6f8e956ea5eb59fb2ee7
hot666666 <hotcs6@naver.com>
2025-02-13 11:18:29 +0900

    feat : GroupDashboardView에서 사이드바 추가 - 사이드바 추가 및 TabNaviType 정의(home, profile) - ProfileSheet 분리 및 사이드바에 추가

--- TodoMate/View/Main/GroupDashboardView.swift
+++ TodoMate/View/Main/GroupDashboardView.swift
@@ -10,12 +10,47 @@ import SwiftUI
 struct GroupDashboardView: View {
     @Environment(DIContainer.self) private var container
     @State private var viewModel: GroupDashboardViewModel
+    /// NavigationSplitView
+    @State private var selectedItem: TabNaviType = .home
+    @State private var columnVisibility: NavigationSplitViewVisibility = .detailOnly
     
     init(viewModel: GroupDashboardViewModel) {
         self._viewModel = State(initialValue: viewModel)
     }
     
     var body: some View {
+        NavigationSplitView(
+            columnVisibility: $columnVisibility,
+            sidebar: {
+                List(selection: $selectedItem) {
+                    sidebar
+                }
+            },
+            detail: {
+                switch selectedItem {
+                case .home:
+                    home
+                case .profile:
+                    profile
+                }
+            })
+    }
+    
+    @ViewBuilder
+    private var sidebar: some View {
+        ForEach(TabNaviType.allCases, id: \.self) { tab in
+            NavigationLink(value: tab) {
+                tab.label
+            }
+            .foregroundColor(.white)
+        }
+    }
+    
+    @ViewBuilder
+    private var home: some View {
+        /// .windowStyle(.hiddenTitleBar) 버그로 인해 추가
+        Color.clear.frame(height: 0)
+        
         ScrollView {
             VStack {
                 ChatBoardView(viewModel: .init(container: container,
@@ -23,15 +58,38 @@ struct GroupDashboardView: View {
                 TodoBoardView(viewModel: .init(container: container,
                                                userInfo: viewModel.userInfo))
             }
+            .padding(.horizontal)
+        }
+    }
+    
+    @ViewBuilder
+    private var profile: some View {
+        ProfileSheetView(viewModel: .init(container: container,
+                                          userInfo: viewModel.userInfo,
+                                          updateGroup: viewModel.fetchGroupUser))
+    }
+}
+extension GroupDashboardView {
+    private enum TabNaviType: String, CaseIterable {
+        case home = "홈"
+        case profile = "계정"
+        
+        var label: Label<Text, Image> {
+            switch self {
+            case .home:
+                return Label(self.rawValue, systemImage: "house")
+            case .profile:
+                return Label(self.rawValue, systemImage: "person.crop.circle")
+            }
         }
     }
 }
 
 #Preview {
-    GroupDashboardView(viewModel: .init(container: .stub, userInfo: .hasGroupStub))
-        .environment(DIContainer.stub)
-        .environment(OverlayManager.stub)
-        .environment(AuthManager.signedInAndHasGroupStub)
-        .frame(width: 400, height: 400)
-    
+    OverlayContainer {
+        GroupDashboardView(viewModel: .init(container: .stub, userInfo: .hasGroupStub))
+            .environment(DIContainer.stub)
+            .environment(AuthManager.signedInAndHasGroupStub)
+            .frame(width: 450, height: 400)
+    }
 }
---
+++ TodoMate/View/Main/ProfileSheet.swift
@@ -0,0 +1,78 @@
+//
+//  Profile.swift
+//  TodoMate
+//
+//  Created by hs on 1/29/25.
+//
+
+import SwiftUI
+
+@Observable
+class ProfileViewModel {
+    private let userInfo: AuthenticatedUser
+    private let userService: UserServiceType
+    private let updateGroup: () async -> Void
+    
+    var user: User? = nil
+    var isLoading: Bool = false
+ 
+    init(container: DIContainer, userInfo: AuthenticatedUser, updateGroup: @escaping () async -> Void) {
+        self.userInfo = userInfo
+        self.userService = container.userService
+        self.updateGroup = updateGroup
+    }
+    
+    @MainActor
+    func fetchUser() async {
+        self.isLoading = true
+        defer { self.isLoading = false }
+        
+        self.user = await userService.fetch(uid: userInfo.id)
+    }
+    
+    func updateGroup() async {
+        await updateGroup()
+    }
+}
+
+struct ProfileSheetView: View {
+    @State private var viewModel: ProfileViewModel
+    
+    init(viewModel: ProfileViewModel) {
+        self._viewModel = State(initialValue: viewModel)
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            userProfile
+            
+            Button {
+                Task { await viewModel.updateGroup() }
+            } label: {
+                Text("그룹 최신화")
+            }
+            
+            SignOutButton()
+        }
+        .task {
+            await viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    var userProfile: some View {
+        if viewModel.isLoading {
+            ProgressView()
+        } else {
+            Text(viewModel.user?.nickname ?? "정보 없음")
+                .font(.largeTitle)
+        }
+    }
+        
+}
+
+#Preview {
+    ProfileSheetView(viewModel: .init(container: .stub, userInfo: AuthenticatedUser.stub, updateGroup: {}))
+        .environment(AuthManager.signedInAndHasGroupStub)
+        .frame(width: 400, height: 400)
+}
