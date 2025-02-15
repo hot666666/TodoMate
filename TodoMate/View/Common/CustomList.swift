3d0ccae7c5fd81783d608fe06b643abf9d7f2193
hot666666 <hotcs6@naver.com>
2025-02-15 21:46:27 +0900

    fix : CustomList 업데이트

--- TodoMate/View/Common/CustomList.swift
+++ TodoMate/View/Common/CustomList.swift
@@ -7,47 +7,39 @@
 
 import SwiftUI
 
-
 // MARK: - CustomList
-/// List 뷰 기본 스타일들을 제외, onMove 기능 추가, rowHeight(동일) 동적 계산으로 스크롤 제거
-struct CustomList<Item: Identifiable & Equatable, ItemView: View>: View {
+/// - List 뷰 기본 스타일들을 제외, onMove 기능 추가
+/// - rowHeight(일정)으로 높이를 동적 계산하여 스크롤 제거
+struct CustomList<Item: Equatable & Identifiable, ItemView: View>: View {
     @State private var rowHeight: CGFloat = 40
-    let items: [Item]
+    
+    @Binding var items: [Item]
     let onMove: (IndexSet, Int) -> Void
     let itemView: (Item) -> ItemView
     
     var body: some View {
-        ScrollViewReader { proxy in
-            List {
-                ForEach(items) { item in
-                    itemView(item)
-                        .background(GeometryReader { proxy in
-                            Color.clear
-                                .onAppear {
-                                    rowHeight = proxy.size.height
-                                }
-                        })
-                        .listRowInsets(EdgeInsets())
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        .padding(.leading, 20)
-                        .overlay(alignment: .leading) {
-                            Divider()
-                                .frame(width: 15)
-                        }
-                        .id(item.id)
-                }
-                .onMove(perform: onMove)
+        List {
+            ForEach(items) { item in
+                itemView(item)
+                    .background(GeometryReader { proxy in
+                        Color.clear
+                            .onAppear {
+                                rowHeight = proxy.size.height
+                            }
+                    })
+                    .listRowInsets(EdgeInsets())
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                    .padding(.leading, 20)
+                    .overlay(alignment: .leading) {
+                        Divider()
+                            .frame(width: 15)
+                    }
             }
-            .frame(height: CGFloat(items.count) * rowHeight)
-            .listStyle(.plain)
-            .scrollContentBackground(.hidden)
-            // MARK: - 첫번째 아이템이 변경될 때, 스크롤이 올라가는 버그를 해결하기 위해 추가
-            .onChange(of: items.first) {
-                if let firstItem = $1 {
-                    proxy.scrollTo(firstItem.id, anchor: .top)
-                }
-            }
+            .onMove(perform: onMove)
         }
+        .frame(height: CGFloat(items.count) * rowHeight)
+        .listStyle(.plain)
+        .scrollContentBackground(.hidden)
     }
 }
