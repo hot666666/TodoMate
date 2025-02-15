d930a92f5ddf6f38eac8e6e33a01a84c44afc1fa
hot666666 <hotcs6@naver.com>
2025-02-16 00:04:21 +0900

    fix : FirestoreTodoRepository - DocumentId를 얻은 DTO를 반환하도록 메서드를 수정(Repository->Service) - TodoServiceType 수정

--- TodoMate/Repository/FirestoreTodoRepository.swift
+++ TodoMate/Repository/FirestoreTodoRepository.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 protocol TodoRepositoryType {
-    func createTodo(todo: TodoDTO) async throws
     func createTodo(_ todo: TodoDTO) async throws -> TodoDTO
     func fetchTodos(userId: String, startDate: Date, endDate: Date) async throws -> [TodoDTO]
     func updateTodo(todo: TodoDTO) async throws
@@ -25,18 +24,10 @@ final class FirestoreTodoRepository: TodoRepositoryType {
 #if !PREVIEW
 extension FirestoreTodoRepository {
     func createTodo(_ todo: TodoDTO) async throws -> TodoDTO {
-        let todoDocRef = reference.todoCollection().document()
-        var newTodo = todo
-        newTodo.id = todoDocRef.documentID
-        try todoDocRef.setData(from: newTodo)
-        return newTodo
-    }
-    
-    func createTodo(todo: TodoDTO) async throws {
-        let todoDocRef = reference.todoCollection().document()
-        var newTodo = todo
-        newTodo.id = todoDocRef.documentID
-        try todoDocRef.setData(from: newTodo)
+        let collectionRef = reference.todoCollection()
+        let newDocReference = try collectionRef.addDocument(from: todo)
+        let document = try await newDocReference.getDocument()
+        return try document.data(as: TodoDTO.self)
     }
     
     func fetchTodos(userId: String, startDate: Date, endDate: Date) async throws -> [TodoDTO] {
