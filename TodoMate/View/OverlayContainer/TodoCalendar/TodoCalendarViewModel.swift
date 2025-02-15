d3d6782d0a9b5bc1497868254e90c0341361db7a
hot666666 <hotcs6@naver.com>
2025-02-16 00:03:17 +0900

    fix : TodoCalendarView, TodoCalendarViewModel 업데이트 - Model create 시, documentId를 반환받아 처리되기에 Task로 감싸서 메서드를 수행하도록 수정

--- TodoMate/View/OverlayContainer/TodoCalendar/TodoCalendarViewModel.swift
+++ TodoMate/View/OverlayContainer/TodoCalendar/TodoCalendarViewModel.swift
@@ -20,6 +20,7 @@ class TodoCalendarViewModel {
     var todos: [Date: [Todo]] = [:]
     var currentDate: Date = .now
     var calendarDays: [CalendarDay] = []
+    var isLoading: Bool = false
     
     init(container: DIContainer, user: User, isMine: Bool, onDismiss: @escaping () -> Void) {
         self.todoService = container.todoService
@@ -35,95 +36,129 @@ extension TodoCalendarViewModel {
     }
     
     func onDrop(data: [TodoTransferData], to newDate: Date) -> Bool {
-        guard isMine else { return false }
+        guard isMine, let oldTodoData = data.first else { return false }
         
-        guard let data = data.first else { return false }
+        let fid = oldTodoData.fid
+        let oldDate = calendar.startOfDay(for: oldTodoData.date)
+        let newDate = calendar.startOfDay(for: newDate)
         
-        let oldDate = calendar.startOfDay(for: data.date)
-        guard let movedTodo = todos[oldDate]?.first(where: { $0.id == data.id }),
-                movedTodo.status != .inProgress else {
-            return false
-        }
+        guard
+            var oldTodo = todos[oldDate]?.first(where: { $0.fid == fid }),
+            oldTodo.status != .inProgress
+        else { return false }
         
-        movedTodo.date = newDate
+        oldTodo.date = newDate
         
-        todoService.update(movedTodo)
+        todoService.update(oldTodo)
         
-        let oldTodoDate = calendar.startOfDay(for: data.date)
-        let newTodoDate = calendar.startOfDay(for: newDate)
+        todos[oldDate, default: []].removeAll { $0.fid == fid }
+        todos[newDate, default: []].append(oldTodo)
         
-        todos[oldTodoDate, default: []].removeAll { $0.fid == movedTodo.fid }
-        todos[newTodoDate, default: []].append(movedTodo)
         return true
     }
 }
 extension TodoCalendarViewModel {
-    func moveMonth(by value: Int) {
+    func moveMonth(by value: Int) async {
         let updatedDate = calendar.addMonths(value, to: currentDate)!
         currentDate = updatedDate
         updateCalendarDays()
-        Task {
-            await fetch()
-        }
+        
+        await fetch()
     }
     
-    func currentMonth() {
+    func currentMonth() async {
         currentDate = .now
         updateCalendarDays()
-        Task {
-            await fetch()
-        }
+        
+        await fetch()
     }
 }
 extension TodoCalendarViewModel {
     @MainActor
     func fetch() async {
+        defer { isLoading = false }
+        
         let startDateOfMonth = calendar.startOfMonth(for: currentDate)
         let startDate = calendar.addDays(-calendar.component(.weekday, from: startDateOfMonth) + 1, to: startDateOfMonth)
         let endDate = calendar.addDays(41, to: startDate).addingTimeInterval(-1)
         
+        isLoading = true
         todos = await todoService.fetchMonth(userId: user.uid, startDate: startDate, endDate: endDate)
     }
-
+    
     @MainActor
     func create(date: Date) async {
         guard isMine else { return }
+        defer { isLoading = false }
+        
         let todo: Todo = .init(date: date, uid: user.uid)
         
-        if let createdTodo = await todoService.create(from: todo) {
-            let todoDate = calendar.startOfDay(for: createdTodo.date)
-            todos[todoDate, default: []].append(createdTodo)
+        isLoading = true
+        guard
+            let createdTodo = await todoService.create(from: todo),
+            !createdTodo.fid.isEmpty
+        else {
+            print("Failed to create todo")
+            return
         }
+        
+        let todoDate = calendar.startOfDay(for: createdTodo.date)
+        todos[todoDate, default: []].append(createdTodo)
     }
     
     @MainActor
     func copy(_ todo: Todo) async {
         guard isMine else { return }
-        if let createdTodo = await todoService.create(from: .init(date: todo.date,
-                                                                  content: todo.content,
-                                                                  detail: todo.detail,
-                                                                  uid: todo.uid)) {
-            let todoDate = calendar.startOfDay(for: createdTodo.date)
-            todos[todoDate, default: []].append(createdTodo)
+        defer { isLoading = false }
+        
+        let copiedTodo: Todo = .copy(from: todo)
+        
+        isLoading = true
+        guard
+            let createdTodo = await todoService.create(from: copiedTodo),
+            !createdTodo.fid.isEmpty
+        else {
+            print("Failed to create todo")
+            return
         }
+        
+        let todoDate = calendar.startOfDay(for: createdTodo.date)
+        todos[todoDate, default: []].append(createdTodo)
     }
     
+    func update(oldDate: Date, newTodo: Todo) {
+        guard isMine else { return }
+        
+        todoService.update(newTodo)
+        
+        let oldDate = calendar.startOfDay(for: oldDate)
+        let newDate = calendar.startOfDay(for: newTodo.date)
+        guard
+            var todosInDate = todos[newDate],
+            let index = todosInDate.firstIndex(where: { $0.fid == newTodo.fid })
+        else {
+            /// 현재 캘린더 뷰에 없는 날짜로 이동한 경우, 기존 날짜의 데이터를 삭제만 수행
+            todos[oldDate, default: []].removeAll { $0.fid == newTodo.fid }
+            return
+        }
+        
+        if oldDate == newDate {
+            todosInDate[index] = newTodo
+            todos[newDate] = todosInDate
+        } else {
+            todos[oldDate, default: []].removeAll { $0.fid == newTodo.fid }
+            todos[newDate, default: []].append(newTodo)
+        }
+    }
+
     func remove(_ todo: Todo) {
         guard isMine else { return }
+        
         todoService.remove(todo)
         
         let todoDate = calendar.startOfDay(for: todo.date)
+        
         todos[todoDate, default: []].removeAll { $0.fid == todo.fid }
-    }
-    
-    func update(oldTodoDate: Date, newTodo: Todo) {
-        guard isMine else { return }
-        todoService.update(newTodo)
-        
-        let newTodoDate = calendar.startOfDay(for: newTodo.date)
-        
-        todos[oldTodoDate, default: []].removeAll { $0.fid == newTodo.fid }
-        todos[newTodoDate, default: []].append(newTodo)
     }
 }
 extension TodoCalendarViewModel {
