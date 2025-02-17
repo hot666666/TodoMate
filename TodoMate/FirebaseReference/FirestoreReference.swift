acc4045a2e3f5cf7d266d5ebb8a15a6d19868d75
hot666666 <hotcs6@naver.com>
2025-02-17 12:12:01 +0900

    feat : StubFirestore 구현 - FirebaseReference에서 실제 Firestore 작업을 묘사 -> CRUD 작업 및 실시간 변화 감지 테스트 수행 가능

--- TodoMate/FirebaseReference/FirestoreReference.swift
+++ TodoMate/FirebaseReference/FirestoreReference.swift
@@ -32,9 +32,77 @@ final class FirestoreReference {
     }
 }
 #else
-class FirestoreReference {
+final class FirestoreReference {
     static let shared = FirestoreReference()
+    let db = StubFirestore()
     
     private init() {}
+}
+
+final class StubFirestore {
+    /// AsyncStream에서 전달받은 continuation을 저장하는 프로퍼티, 단일 구독자용
+    private(set) var continuation: AsyncStream<DatabaseChange<TodoDTO>>.Continuation? = nil
+    private(set) var todos: [TodoDTO] = []
+    
+//    private var users: [UserDTO] = []
+//    private var chats: [ChatDTO] = []
+//    private var groups: [GroupDTO] = []
+    
+    
+    init() {}
+    
+    func todoStream() -> AsyncStream<DatabaseChange<TodoDTO>> {
+        AsyncStream { continuation in
+            /// 구독 시 기존 todos 상태를 초기 이벤트로 전달
+            self.todos.forEach { continuation.yield(.added($0)) }
+            
+            self.continuation = continuation
+        }
+    }
+    
+    func todoStreamTermination() {
+        continuation?.finish()
+        continuation = nil
+    }
+    
+    func create(todo: TodoDTO) throws -> TodoDTO {
+        var newTodo = todo
+        newTodo.id = UUID().uuidString
+        
+        todos.append(newTodo)
+        guard let continuation = continuation else {
+            throw NSError(domain: "Continuation is nil", code: 0)
+        }
+        
+        continuation.yield(.added(newTodo))
+        return newTodo
+    }
+    
+    func read() -> [TodoDTO] {
+        return todos
+    }
+    
+    func update(todo: TodoDTO) throws {
+        guard let index = todos.firstIndex(where: { $0.id == todo.id }) else { return }
+        todos[index] = todo
+        
+        guard let continuation = continuation else {
+            throw NSError(domain: "Continuation is nil", code: 0)
+        }
+        
+        continuation.yield(.modified(todo))
+    }
+    
+    func delete(todoId: String) throws {
+        guard let index = todos.firstIndex(where: { $0.id == todoId }) else { return }
+        
+        let todo = todos.remove(at: index)
+        
+        guard let continuation = continuation else {
+            throw NSError(domain: "Continuation is nil", code: 0)
+        }
+        
+        continuation.yield(.removed(todo))
+    }
 }
 #endif
