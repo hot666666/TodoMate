bc762b26c58d8c308723a290d16431b190fe0d92
hot666666 <hotcs6@naver.com>
2025-03-01 17:53:49 +0900

    feat : CurrentUserStore 생성 - user 프로퍼티를 통해 로그인 된 유저의 정보를 사용

---
+++ TodoMate/Store/CurrentUserStore.swift
@@ -0,0 +1,73 @@
+//
+//  CurrentUserStore.swift
+//  TodoMate
+//
+//  Created by hs on 3/1/25.
+//
+
+import Observation
+
+@Observable
+final class CurrentUserStore {
+    private let localDataManager: LocalDataManagerType
+    private let authService: AuthServiceType
+    private let userInfoService: UserInfoServiceType
+    /// Cached user information
+    private var _authenticatedUser: AuthenticatedUser?
+    
+    private(set) var user: AuthenticatedUser? {
+        get { _authenticatedUser }
+        set {
+            _authenticatedUser = newValue
+            userInfoService.saveUserInfo(newValue)
+        }
+    }
+    private(set) var state: State = .signedOut
+    
+    init(container: DIContainer = .stub) {
+        self.authService = container.authService
+        self.localDataManager = container.localDataManager
+        self.userInfoService = container.userInfoService
+        
+        self._authenticatedUser = self.userInfoService.loadUserInfo()
+    }
+    
+    @MainActor
+    func signIn() async {
+        state = .loading
+        
+        if let signedInUser = await authService.signIn() {
+            let aUser = AuthenticatedUser(uid: signedInUser.uid, gid: signedInUser.gid)
+            user = aUser
+            state = .signedIn(aUser)
+        } else {
+            state = .signedOut
+        }
+    }
+    
+    @MainActor
+    func signOut() async {
+        defer {
+            user = nil
+            state = .signedOut
+        }
+        state = .loading
+        
+        await authService.signOut()
+        await localDataManager.removeAllTodoEntity()
+    }
+    
+    func updateUserGroup(_ gid: String) {
+        if let user = user {
+            self.user = AuthenticatedUser(uid: user.uid, gid: gid)
+        }
+    }
+    
+}
+extension CurrentUserStore {
+    enum State {
+        case signedOut
+        case signedIn(AuthenticatedUser)
+        case loading
+    }
+}
