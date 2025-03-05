ef87aaba6eb1720bec0e81edc1b64c470cb0d3ea
hot666666 <hotcs6@naver.com>
2025-03-05 21:45:19 +0900

    chore : Xcode 테스트타겟 재생성

---
+++ TodoMateTests/TodoMateTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodoMateTests.swift
+//  TodoMateTests
+//
+//  Created by hs on 3/5/25.
+//
+
+import Testing
+@testable import TodoMate
+
+struct TodoMateTests {
+    @Test
+    func example() async throws {
+        #expect(1+1==2, "1+1=2")
+    }
+}
