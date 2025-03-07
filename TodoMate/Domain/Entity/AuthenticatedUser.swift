193689cfec24a402d402c7c7eac743f540333145
hot666666 <hotcs6@naver.com>
2025-03-07 15:15:19 +0900

    fix : AuthenticatedUser 업데이트 - from 메서드 추가(User->AuthenticatedUser)

--- TodoMate/Domain/Entity/AuthenticatedUser.swift
+++ TodoMate/Domain/Entity/AuthenticatedUser.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 
-// TODO: - name add
 struct AuthenticatedUser: Codable {
     let uid: String
     let gid: String
     
     static let stub = AuthenticatedUser(uid: User.stub[0].uid, gid: "")
     static let hasGroupStub = AuthenticatedUser(uid: User.stub[0].uid, gid: UserGroup.stub.id)
+}
+extension AuthenticatedUser {
+    static func from(_ user: User) -> AuthenticatedUser {
+        AuthenticatedUser(uid: user.uid, gid: user.gid)
+    }
 }
