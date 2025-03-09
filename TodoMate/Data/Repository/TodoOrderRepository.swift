fab2f5a68fcfd048386fe08a8b3e00600776a283
hot666666 <hotcs6@naver.com>
2025-03-09 09:20:07 +0900

    chore : TodoOrderRepository 업데이트 - 릴리즈가 아닌 경우, 임시저장 적용

--- TodoMate/Data/Repository/TodoOrderRepository.swift
+++ TodoMate/Data/Repository/TodoOrderRepository.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 class TodoOrderRepository: TodoOrderRepositoryType {
-    private let userDefaults = UserDefaults.standard
+    private let userDefaults: UserDefaults
     /// PREVIEW용 키 따로 존재
     private let todoOrderDateKey = Const.TodoOrderDateKey
     private let todoOrderKey = Const.TodoOrderKey
+    
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+#if DEBUG || PREVIEW
+        userDefaults.removePersistentDomain(forName: "TestUserDefaults")
+#endif
+    }
     
     func saveDate(_ date: Date) {
         userDefaults.set(date, forKey: todoOrderDateKey)
