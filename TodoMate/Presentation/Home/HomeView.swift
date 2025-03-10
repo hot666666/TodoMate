13856d75119e4801caa563201ffa5f0be98d9611
hot666666 <hotcs6@naver.com>
2025-03-10 14:15:14 +0900

    feat : HomeView 단축키 사용 기능 추가 - 등록된 Notification.Name을 바탕으로 Command+N에 대한 동작수행 - Combine 파이프라인을 통한 debounce 추가

--- TodoMate/Presentation/Home/HomeView.swift
+++ TodoMate/Presentation/Home/HomeView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import Combine
 
 struct HomeView: View {
     @Environment(DIContainer.self) private var container
+    @Environment(OverlayManager.self) private var overlayManager
+    @State private var cancellable: AnyCancellable?
     
     // TODO: - GroupDashboard에서 그룹 유저를 패치하는 문제
     let userInfo: AuthenticatedUser
@@ -23,5 +26,27 @@ struct HomeView: View {
             }
             .padding(.horizontal)
         }
+        .onReceive(NotificationCenter.default.publisher(for: .createUserTodoTriggered)) { _ in
+            /// Debounce 적용 (0.5초 내 중복 호출 무시)
+            cancellable?.cancel()
+            cancellable = Just(())
+                .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+                .sink { _ in
+                    Task {
+                        await createUserTodo()
+                    }
+                }
+        }
+        .onAppear {
+            cancellable = nil
+        }
+    }
+    
+    @MainActor
+    private func createUserTodo() async {
+        guard let craetedTodo = await container.todoService.create(from: .init(uid: userInfo.uid)) else { return }
+        overlayManager.push(.todo(craetedTodo, isMine: true, update: { _, updatedTodo in
+            container.todoService.update(updatedTodo)
+        }))
     }
 }
