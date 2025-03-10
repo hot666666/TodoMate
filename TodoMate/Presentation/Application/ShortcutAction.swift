fc82d3db12ebe78643964fa4868abae96b9de8d9
hot666666 <hotcs6@naver.com>
2025-03-10 16:09:35 +0900

    chore : ShortcutAction 분리

---
+++ TodoMate/Presentation/Application/ShortcutAction.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutAction.swift
+//  TodoMate
+//
+//  Created by hs on 3/10/25.
+//
+
+import SwiftUI
+
+enum ShortcutAction: String {
+    case createUserTodo
+    case closeOverlay
+}
+
+extension Notification.Name {
+    static let shortcutAction = Notification.Name("todoMateShortcutAction")
+}
