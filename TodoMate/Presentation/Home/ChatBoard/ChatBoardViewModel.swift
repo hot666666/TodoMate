5e90b801e5484b2feb00e6c3f7475d914c4d80a9
hot666666 <hotcs6@naver.com>
2025-03-10 10:23:24 +0900

    chore : Pesestation 폴더 정리

---
+++ TodoMate/Presentation/Home/ChatBoard/ChatBoardViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  ChatBoardViewModel.swift
+//  TodoMate
+//
+//  Created by hs on 1/22/25.
+//
+
+import SwiftUI
+
+@Observable
+class ChatBoardViewModel {
+    private let chatService: ChatServiceType
+    private let chatStreamProvider: ChatStreamProviderType
+    private let userInfo: AuthenticatedUser
+    
+    var chats: [Chat] = []
+    
+    init(container: DIContainer, userInfo: AuthenticatedUser) {
+        self.chatService = container.chatService
+        self.chatStreamProvider = container.chatStreamProvider
+        self.userInfo = userInfo
+    }
+}
+extension ChatBoardViewModel {
+    func observeChanges() async {
+        for await change in chatStreamProvider.createChatStream() {
+            print("[Observed Chat change in FirebaseFirestore] - ", change)
+            await handleDatabaseChange(change)
+        }
+    }
+    
+    @MainActor
+    private func handleDatabaseChange(_ change: DatabaseChange<Chat>) {
+        switch change {
+        case .added(let chat):
+            if !chats.contains(where: { $0.fid == chat.fid }) {
+                chats.append(chat)
+            }
+        case .modified(let chat):
+            /// 내가 입력 중이던 요소는 업데이트 무시
+            guard (chat.lastModifiedUser != userInfo.uid) else { return }
+            if let index = chats.firstIndex(where: { $0.fid == chat.fid }) {
+                chats[index] = chat
+            }
+        case .removed(let chat):
+            if let index = chats.firstIndex(where: { $0.fid == chat.fid }) {
+                chats.remove(at: index)
+            }
+        }
+    }
+}
+extension ChatBoardViewModel {
+    /// 최초 observeChanges() 시, 모든 데이터를 다 fetch를 수행해서 사용되지 않음
+    @MainActor
+    func fetchChat() async {
+        chats = await chatService.fetch()
+    }
+    
+    func createChat(with url: String? = nil) {
+        let chat: Chat = .init(lastModifiedUser: userInfo.uid)
+        
+        /// 이미지 업로드 시
+        if let url = url {
+            chat.content = url
+            chat.isImage = true
+        }
+        chatService.create(chat)
+        
+#if PREVIEW
+        chat.fid = UUID().uuidString
+        chats.append(chat)
+#endif
+    }
+    
+    func updateChat(_ chat: Chat) {
+        chat.lastModifiedUser = userInfo.uid
+        chatService.update(chat)
+    }
+    
+    func removeChat(_ chat: Chat) {
+        chatService.remove(chat)
+        
+#if PREVIEW
+        if let index = chats.firstIndex(where: { $0.fid == chat.fid }) {
+            chats.remove(at: index)
+        }
+#endif
+    }
+}
---
+++ TodoMate/Presentation/Home/TodoBoard/TodoBoardView.swift
@@ -0,0 +1,94 @@
+//
+//  TodoBoardView.swift
+//  TodoMate
+//
+//  Created by hs on 12/28/24.
+//
+
+import SwiftUI
+
+// MARK: - TodoBoardView
+struct TodoBoardView: View {
+    @Environment(DIContainer.self) private var container
+    @State private var viewModel: TodoBoardViewModel
+    private let users: [User]
+    
+    init(viewModel: TodoBoardViewModel, users : [User]) {
+        self._viewModel = State(initialValue: viewModel)
+        self.users = users
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(users, id: \.uid) { user in
+                ExpandableView(
+                    storageKey: user.uid,
+                    header: { header(for: user) },
+                    content: { content(for: user) }
+                )
+            }
+            
+            if users.isEmpty {
+                placeholder
+            }
+            
+            Spacer(minLength: 50)
+        }
+        .task {
+            await viewModel.fetchTodos()
+            await viewModel.observeChanges()
+        }
+    }
+    
+    @ViewBuilder
+    private func header(for user: User) -> some View {
+        HStack {
+            Text(user.nickname)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private func content(for user: User) -> some View {
+        TodoBoxView(
+            user: user,
+            isMine: viewModel.isMe(user),
+            todos: viewModel.todosBinding(for: user),
+            createTodo: viewModel.createTodo,
+            deleteTodo: viewModel.deleteTodo,
+            updateTodo: viewModel.updateTodo,
+            moveTodo: viewModel.moveTodo
+        )
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    private var placeholder: some View {
+        GroupBox {
+            VStack(alignment: .leading) {
+                Label("오늘의 투두", systemImage: "list.dash")
+                Divider()
+                    .padding(.bottom)
+            }
+            .padding(5)
+        }
+        .opacity(0.7)
+        .padding(.horizontal, 20)
+    }
+}
+
+
+#Preview {
+    OverlayContainer {
+        ScrollView {
+            VStack{
+                TodoBoardView(viewModel: .init(container: .stub, userInfo: .stub),
+                              users: User.stub)
+                Spacer()
+            }
+        }
+        .environment(DIContainer.stub)
+        .frame(width: 400, height: 600)
+        .padding()
+    }
+}
