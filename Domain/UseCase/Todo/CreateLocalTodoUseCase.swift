e555f098cc447c93941e3f560dfc16e472e9a945
hot666666 <hotcs6@naver.com>
2026-01-13 12:25:03 +0900

    chore: 미사용 import문 정리

--- Domain/UseCase/Todo/CreateLocalTodoUseCase.swift
+++ Domain/UseCase/Todo/CreateLocalTodoUseCase.swift
@@ -5,8 +5,6 @@
 //  Created by hs on 07/02/25.
 //
 
-import Foundation
-
 protocol CreateLocalTodoUseCase {
   func run(_ todo: Todo) async throws
 }
--- Domain/UseCase/Todo/DeleteLocalTodoUseCase.swift
+++ Domain/UseCase/Todo/DeleteLocalTodoUseCase.swift
@@ -5,8 +5,6 @@
 //  Created by hs on 07/02/25.
 //
 
-import Foundation
-
 protocol DeleteLocalTodoUseCase {
   func run(_ todoId: String) async throws
 }
