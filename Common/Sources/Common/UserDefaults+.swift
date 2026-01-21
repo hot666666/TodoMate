4a52afa0b2ea725b5282d1784a2f67797508b70e
hot666666 <hotcs6@naver.com>
2026-01-21 18:56:16 +0900

    chore: move date formatting to a `Date` extension and consolidate `UserDefaults` extensions.

--- Common/Sources/Common/UserDefaults+.swift
+++ Common/Sources/Common/UserDefaults+.swift
@@ -1,11 +1,50 @@
 //
-//  UserDefaultsKey.swift
-//  Common
+//  UserDefaults+.swift
+//  TodoMate
 //
-//  Created by agent on 1/14/26.
+//  Created by agent on 1/6/26.
 //
 
-/// UserDefaults 키를 중앙에서 관리하는 enum
+import Foundation
+
+// MARK: - UserDefaults Extension
+
+public extension UserDefaults {
+  /// In-memory UserDefaults for preivews/tests
+  @MainActor static let preview: UserDefaults = {
+    let suiteName = "preview"
+    let defaults = UserDefaults(suiteName: suiteName)!
+    defaults.removePersistentDomain(forName: suiteName)
+    return defaults
+  }()
+
+  func set(_ value: Any?, for key: UserDefaultsKey) {
+    set(value, forKey: key.rawValue)
+  }
+
+  func string(for key: UserDefaultsKey) -> String? {
+    string(forKey: key.rawValue)
+  }
+
+  func bool(for key: UserDefaultsKey, default defaultValue: Bool = false) -> Bool {
+    object(forKey: key.rawValue) as? Bool ?? defaultValue
+  }
+
+  func double(for key: UserDefaultsKey) -> Double {
+    double(forKey: key.rawValue)
+  }
+
+  func data(for key: UserDefaultsKey) -> Data? {
+    data(forKey: key.rawValue)
+  }
+
+  func removeObject(for key: UserDefaultsKey) {
+    removeObject(forKey: key.rawValue)
+  }
+}
+
+// MARK: - UserDefaults Keys
+
 public enum UserDefaultsKey: String {
   // MARK: - App Migration
 
